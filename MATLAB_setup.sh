64d7f73081854e8bdf8147b588d3f20c8b7c4207
Latané Bullock <1prlabu1@gmail.com>
2022-03-14 17:21:40 -0400

    Update MATLAB_setup.sh

--- MATLAB_setup.sh
+++ MATLAB_setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/altmany/export_fig.git
-git glone https://github.com/scottclowe/cbrewer2.git
+git clone https://github.com/scottclowe/cbrewer2.git
 git clone https://github.com/raacampbell/sigstar.git
 git clone https://github.com/RainCloudPlots/RainCloudPlots.git
